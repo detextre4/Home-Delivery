a8b9c3d3d85cdcaaf9a99d63210a1ee1f45f755a
detextre4 <detextre4@gmail.com>
2022-05-26 21:59:01 -0400

    update

--- src/main.js
+++ src/main.js
@@ -1,44 +1,32 @@
+import * as VueGoogleMaps from 'vue2-google-maps';
 import "vue-toastification/dist/index.css";
 import Toast from "vue-toastification";
 import {i18n} from '@/plugins/i18n'
-import VueAnimateOnScroll from 'vue-animate-onscroll';
 import VueApexCharts from 'vue-apexcharts'
 import router from './Routes'
 import store from './store/index'
 import Vue from 'vue'
+import VueGeolocation from 'vue-browser-geolocation';
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
 // google maps
-import * as VueGoogleMaps from 'vue2-google-maps';
 Vue.use(VueGoogleMaps, {
   load: {
     key: 'AIzaSyB8dExdQtd6WILpKT57uF2boPp8VyCIufk',
   },
 })
 // geolocation
-import VueGeolocation from 'vue-browser-geolocation';
 Vue.use(VueGeolocation);
-
-
+// apex charts
 Vue.use(VueApexCharts)
-
 Vue.component('apexchart', VueApexCharts)
-
-Vue.use(VueAnimateOnScroll);
-
 // Axios
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 Vue.use(VueAxios, axios)
-
+// toast
 Vue.use(Toast);
-
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyB7OXmzfQYua_1LEhRdqsoYzyJOPh9hGLg',
-  },
-});
 
 Vue.config.productionTip = false
 
